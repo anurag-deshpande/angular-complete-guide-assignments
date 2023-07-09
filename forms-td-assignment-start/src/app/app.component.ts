c3981acd704fe5e915f5c59114f2e963f8b20b69
anurag-deshpande <an2andcorp@gmail.com>
2023-07-09 14:20:15 +0530

    Template Driven forms assignment 6

---
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  @ViewChild('f', {static: true}) basicForm: NgForm;
+
+  submitForm() {
+    console.log(this.basicForm);
+  }
+}
